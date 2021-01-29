4bbbe19e2000ad5833b16ce1388c60403b37a45b
Rosalia Contreras <rosaliacontrerasmoreira@gmail.com>
2021-01-29 13:05:13 +0100

    Fix some issues

--- create_tables.py
+++ create_tables.py
@@ -1,6 +1,10 @@
 from cql_queries import create_table_queries, drop_table_queries
 
 def create_cluster():
+    '''
+    Function that makes a connection to a Cassandra instance in local machine
+    Returns the current session
+    '''  
     # This should make a connection to a Cassandra instance your local machine 
     # (127.0.0.1)
     from cassandra.cluster import Cluster
@@ -11,7 +15,9 @@ def create_cluster():
     return session
             
 def create_keyspace():
-    '''function that creates the database and set the keyspace. Returns the current session'''  
+    '''Function that creates the database and set the keyspace. 
+        Returns the current session
+    '''  
     session = create_cluster()
     session.execute("CREATE KEYSPACE IF NOT EXISTS udacity WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }")
     session.set_keyspace('udacity')
@@ -20,8 +26,8 @@ def create_keyspace():
 
 def create_database():
     """
-    - Creates and connects to the db_name database
-    - Returns the session
+    Function that creates and set the keyspace to the udacity database
+    Returns the current session
     """
     session = create_keyspace()
     return session
@@ -34,29 +40,27 @@ def create_tables(session):
     """
     for query in create_table_queries:
         session.execute(query)
-        
 
 def drop_tables(session):
     """
     Drops each table using the queries in `drop_table_queries` list.
+    Args:
+        session: current session
     """
     for query in drop_table_queries:
         session.execute(query)
         
 def main():
     """
-    - Drop (if exists) and creates the sparkify database. 
-    - Establishes connection with the sparkify database and gets cursor to it.  
-    - Drops all the tables.  
+    - Drop (if exists) and creates the udacity database. 
+    - Set the keyspace and the cluster
+    - Drops all the tables if they exist 
     - Creates all tables needed. 
-    - Finally, closes the connection. 
+    - Finally, closes the session. 
     """
     session = create_database()
-    
     drop_tables(session)
     create_tables(session)
-
-    
 
 if __name__ == "__main__":
     main()
