06f6159929a86ced75e7cc2b1d9502812619cca4
Rosalia Contreras <rosaliacontrerasmoreira@gmail.com>
2021-01-29 13:18:57 +0100

    Fix some issues

--- cql_queries.py
+++ cql_queries.py
@@ -1,30 +1,35 @@
 #DROP TABLES
-events_library_table_drop = "DROP TABLE IF EXISTS events_library0"
-events_library1_table_drop = "DROP TABLE IF EXISTS events_library1"
-events_library2_table_drop = "DROP TABLE IF EXISTS events_library2"
+artist_song_by_session_table_drop = "DROP TABLE IF EXISTS artist_song_by_session"
+artist_by_session_table_drop = "DROP TABLE IF EXISTS artist_by_session"
+song_playlist_session_table_drop = "DROP TABLE IF EXISTS song_playlist_session"
 
 
-#Create tables events_library
-query = "CREATE TABLE IF NOT EXISTS events_library0"
+#Create tables
+query = "CREATE TABLE IF NOT EXISTS artist_song_by_session"
 query = query + "(sessionId int, itemInSession int, artist varchar, song varchar, length float, PRIMARY KEY (sessionId, itemInSession))"
 
-query1 = "CREATE TABLE IF NOT EXISTS events_library1"
+query1 = "CREATE TABLE IF NOT EXISTS artist_by_session"
 query1 = query1 + "(userId int, sessionId int, itemInSession int, artist varchar, song varchar, user varchar, PRIMARY KEY ((userid, sessionId), itemInSession))"
 
-query2 = "CREATE TABLE IF NOT EXISTS events_library2"
-query2 = query2 + "(song varchar, user varchar, PRIMARY KEY (song))"
+query2 = "CREATE TABLE IF NOT EXISTS song_playlist_session"
+query2 = query2 + "(song varchar, user_id int, user varchar, PRIMARY KEY (song, user_id))"
 
 #####################################################################
 #CREATE QUERIES 
 #####################################################################
-query_events_library = "select artist, song, length from events_library0 WHERE sessionId = 338 AND itemInSession = 4"
-query_events_library1 = "select artist, song, user from events_library1 WHERE userId = 10"
-query_events_library2 = "select user from events_library2 WHERE song = 'All Hands Against His Own'"
+#Here the Primary Key has two fields: sessionId is the partition key, and itemInSession is clustering key. Partitioning is done by sessionId and within that partition, rows are ordered by the itemInSession.
+query_artist_song_session = "SELECT artist, song, length FROM artist_song_by_session WHERE sessionId = 338 AND itemInSession = 4"
+
+#Here the Primary Key has three fields: userId and sessionId are the partition key, and itemInSession is clustering key. Partitioning is done by userId and sessionId and within that partition, rows are ordered by the itemInSession.
+query_artist_session = "SELECT artist, song, itemInSession, user, sessionId FROM artist_by_session WHERE userId = 10 AND sessionId = 182"
+
+#Here the Primary Key has two fields: song is the partition key, and userId is clustering key. Partitioning is done by song and within that partition, rows are ordered by the userId.
+query_song_playlist_session = "SELECT user FROM song_playlist_session WHERE Song = 'All Hands Against His Own'"
 
 #####################################################################
 # QUERY LISTS
 #####################################################################
 create_table_queries = [query, query1, query2]
-select_table_queries = [query_events_library, query_events_library1, query_events_library2]
-drop_table_queries = [events_library_table_drop, events_library1_table_drop, events_library2_table_drop]
+select_table_queries = [query_artist_song_session, query_artist_session, query_song_playlist_session]
+drop_table_queries = [artist_song_by_session_table_drop, artist_by_session_table_drop, song_playlist_session_table_drop]
 
